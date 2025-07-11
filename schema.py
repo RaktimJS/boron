b04791cf427472b1bd58a2e31cdb2290822be09e
RaktimJS <raktimunreal4@gmail.com>
2025-07-11 11:14:34 +0530

    Schema handler briefing update

--- schema.py
+++ schema.py
@@ -5,7 +5,9 @@ This module contains all functions for schema handling.
 """
 The schema definition for that particular JSON is stored with the JSON file's path
 as an object, and the JSON storing the cache is an array-type JSON at the topmost
-level. Meaning, cache.json is an array that stores multiple JSON objects.
+level. Meaning, cache.json is an array that stores multiple JSON objects, each one
+containing 2 attributes, "path" and "schema", where "path" stores the file path of
+the JSON and "schema" stores the schema definition of the "JSON".
 """
 
 
