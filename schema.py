bf0b86bc3e11506cfa027304ad4ab6c475c7d24b
RaktimJS <raktimunreal4@gmail.com>
2025-07-12 06:44:28 +0530

    Schema fetcher function definition in schema.py

--- schema.py
+++ schema.py
@@ -13,3 +13,30 @@ the JSON and "schema" stores the schema definition of the "JSON".
 
 
 
+import json, os
+
+
+
+def fetcher(filePath:str):
+        """
+        This function fetches the schema of the JSON (i.e, filePath) if it has been cached in 'cache.json'.
+
+        NOTE: This function is not a validator or a definer.
+        """
+
+        try:
+                with open("cache.json", "r") as cache:
+                        cacheData = json.load(cache)
+        except json.JSONDecodeError:
+                print("'cache.json' is corrupted")
+                return None
+        except Exception as e:
+                print("Unexpected error")
+                return None
+
+
+        for i in cacheData:
+                if filePath in os.path.normpath(i["path"]):
+                        schema = i["schema"]
+                        print(schema)
+                        return schema
